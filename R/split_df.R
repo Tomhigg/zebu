31ee7df717a5784dea0350bcdc0b696d69b1a146
unknown <55110140@ASN-ACA6196.ad.mmu.ac.uk>
2015-07-24 11:26:20 +0100

    fixed split_df error

--- R/split_df.R
+++ R/split_df.R
@@ -6,15 +6,13 @@
 #' @param dataframe The dataframe to be split
 #' @param seed Option to set seet (random number generator) for reproducability, defaults to null
 #' 
-#' @return A list of training and validation dataframes, extract usiing training  <- input$trainset or input$valset
+#' @return A list of training and validation dataframes, extract usiing training  <- input$trainset or input$testset
 
 
 split_df <- function(dataframe, seed=NULL) {
   if (!is.null(seed)) set.seed(seed)
   index <- 1:nrow(dataframe)
   trainindex <- sample(index, trunc(length(index)/2))
-  valset <- dataframe[trainindex, ]
+  trainset <- dataframe[trainindex, ]
   testset <- dataframe[-trainindex, ]
-  x <- list(valset=valset,testset=testset)
-  return(x)
-}
+  list(trainset=trainset,testset=testset)}
